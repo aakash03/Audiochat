5a0b6e0a3f09966cf9e793afe72dfaddb8939be5
aakash03 <aakashmalhotra96@gmail.com>
2016-02-12 09:37:03 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
 
-	$servername = "br-cdbr-azure-south-a.cloudapp.net:3306";
-	$username = "b362327b01741d";
-	$password = "545960e6";
+	$servername = "localhost";
+	$username = "root";
+	$password = "password";
 	
 	$conn = mysqli_connect($servername, $username, $password,'audiochat');
 	
